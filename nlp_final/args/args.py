86a835509192b39e8ee8fcd51d14e7a41dec7e5d
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-04-12 08:38:51 -0500

    Add arguements

--- nlp_final/args/args.py
+++ nlp_final/args/args.py
@@ -1,4 +1,5 @@
 from argparse import ArgumentParser, Namespace
+from pathlib import Path
 
 from . import AlphabeticalOrderHelpFormatter, authors
 
@@ -11,4 +12,23 @@ def getArgs() -> Namespace:
         epilog=f"Authors: {','.join(authors)}",
         formatter_class=AlphabeticalOrderHelpFormatter,
     )
+
+    parser.add_argument(
+        "--mode",
+        default="train",
+        type=str,
+        choices=["train", "test"],
+        required=False,
+        help="Either train models or test models",
+    )
+
+    parser.add_argument(
+        "-o",
+        "--output",
+        default=Path("model"),
+        type=Path,
+        required=False,
+        help="Model folder to store/ read models from",
+    )
+
     return parser.parse_args()
