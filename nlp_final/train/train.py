615242bdd76af623c28daff970dd044a495ed37b
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-04-13 09:22:19 -0500

    Optimize the training process

--- nlp_final/train/train.py
+++ nlp_final/train/train.py
@@ -9,8 +9,11 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import GridSearchCV
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline, make_pipeline
+from sklearnex import patch_sklearn
 
 from nlp_final.data import dataHandler
+
+patch_sklearn()
 
 
 def main(args: Namespace) -> None:
@@ -31,26 +34,20 @@ def main(args: Namespace) -> None:
 def trainMultinomialNaiveBayes(
     x: ndarray, y: ndarray, validationX: ndarray, validationY: ndarray
 ) -> None:
-    ngramRanges: List[Tuple[int, int]] = list(combinations(iterable=range(1, 11), r=2))
-
     parameters: dict[str, List[Any]] = {
-        "tfidfvectorizer__analyzer": ["word", "char"],
-        "tfidfvectorizer__binary": [True, False],
         "tfidfvectorizer__decode_error": ["ignore"],
-        "tfidfvectorizer__lowercase": [True, False],
-        "tfidfvectorizer__ngram_range": ngramRanges,
+        "tfidfvectorizer__lowercase": [False, True],
+        "tfidfvectorizer__ngram_range": [(1, 3)],
         "tfidfvectorizer__norm": ["l1", "l2"],
-        "tfidfvectorizer__smooth_idf": [True, False],
-        "tfidfvectorizer__stop_words": ["english", None],
-        "tfidfvectorizer__strip_accents": ["ascii", "unicode", None],
-        "multinomialnb__alpha": [1.0, 0, 0.5, 1.5, 2.0, 0.1],
-        "multinomialnb__fit_prior": [True, False],
-        "multinomialnb__force_alpha": [True, False],
+        "multinomialnb__alpha": [1.0, 0.5, 1.5],
+        "multinomialnb__force_alpha": [True],
     }
 
     pipeline: Pipeline = make_pipeline(TfidfVectorizer(), MultinomialNB())
 
-    gsvc: GridSearchCV = GridSearchCV(estimator=pipeline, param_grid=parameters)
+    gsvc: GridSearchCV = GridSearchCV(
+        estimator=pipeline, param_grid=parameters, verbose=2, n_jobs=10
+    )
 
     gsvc.fit(X=x, y=y)
 
