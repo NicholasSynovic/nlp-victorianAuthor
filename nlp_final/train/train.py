3889f76453ec0ef4db3bfa70ff35186cae139717
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-04-13 12:34:10 -0500

    Remove imports

--- nlp_final/train/train.py
+++ nlp_final/train/train.py
@@ -1,15 +1,8 @@
 from argparse import Namespace
-from pathlib import Path
-from typing import Any, List
+from typing import List
 
-from joblib import dump
 from numpy import ndarray
 from pandas import DataFrame
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.model_selection import GridSearchCV
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.pipeline import Pipeline, make_pipeline
-from sklearn.svm import SVC
 
 from nlp_final.data import dataHandler
 from nlp_final.train import vectorizer
