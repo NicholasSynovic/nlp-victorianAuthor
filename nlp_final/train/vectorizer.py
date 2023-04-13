2002054bc458aba00d038d2c9b63e3a766d12ca2
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-04-13 16:38:20 -0500

    Do not save vectorizers to files

--- nlp_final/train/vectorizer.py
+++ nlp_final/train/vectorizer.py
@@ -1,30 +1,25 @@
-from argparse import Namespace
-from pathlib import Path
-from pickle import dump
-
 from gensim.models import FastText, Word2Vec
 from numpy import ndarray
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 
-def trainTFIDF(x: ndarray, outputPath: Path) -> None:
+def trainTFIDF(x: ndarray) -> TfidfVectorizer:
     tfidf: TfidfVectorizer = TfidfVectorizer(
         decode_error="ignore", lowercase=False, ngram_range=(1, 3), norm="l2"
     )
 
     print("Training TF-IDF vectorizer...")
     tfidf.fit(raw_documents=x)
-    with open(Path(outputPath, "tfidf.pickle"), "wb") as outFile:
-        dump(obj=tfidf, file=outFile)
+    return tfidf
 
 
-def trainWord2Vec(x: ndarray, outputPath: Path) -> None:
+def trainWord2Vec(x: ndarray) -> Word2Vec:
     print("Training Word2Vec vectorizer...")
     w2v: Word2Vec = Word2Vec(sentences=x, vector_size=500)
-    w2v.save(Path(outputPath, "word2vec.gensim").absolute().__str__())
+    return w2v
 
 
-def trainFastText(x: ndarray, outputPath: Path) -> None:
+def trainFastText(x: ndarray) -> FastText:
     print("Training FastText vectorizer...")
     ft: FastText = FastText(sentences=x, vector_size=500)
-    ft.save(Path(outputPath, "fasttext.gensim").absolute().__str__())
+    return ft
