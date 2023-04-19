5bbe16e02b97c68190ba35aa8f3d0b1d1caee3e8
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-04-19 18:18:59 -0500

    Add code to train MLP

--- nlp_final/common.py
+++ nlp_final/common.py
@@ -1,7 +1,19 @@
 from pathlib import Path
+from typing import Tuple
+
+from gensim.models import FastText, Word2Vec
+from joblib import load
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 trainingDataPath: Path = Path("../dataset/training.csv")
 
 tfidfPath: Path = Path("../models/tfidf.joblib").resolve()
 word2vecPath: Path = Path("../models/word2vec.gensim").resolve()
 fasttextPath: Path = Path("../models/fasttext.gensim").resolve()
+
+
+def loadVectorizers() -> Tuple[FastText, TfidfVectorizer, Word2Vec]:
+    fasttext: FastText = FastText.load(fasttextPath.__str__())
+    tfidf: TfidfVectorizer = load(filename=tfidfPath)
+    word2vec: Word2Vec = Word2Vec.load(word2vecPath.__str__())
+    return (fasttext, tfidf, word2vec)
