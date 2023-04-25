3c48c1184fc5a81d3ea05ed1573728fcd71df215
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-04-25 12:08:25 -0500

    Improve the csv reading to read from the original dataset and convert on the fly

--- downloadData.bash
+++ downloadData.bash
@@ -4,6 +4,3 @@ wget -O data.zip https://archive.ics.uci.edu/ml/machine-learning-databases/00454
 
 unzip data.zip
 rm data.zip
-
-iconv dataset/Gungor_2018_VictorianAuthorAttribution_data-train.csv -c -t UTF-8 -o dataset/training.csv
-iconv dataset/Gungor_2018_VictorianAuthorAttribution_data.csv -c -t UTF-8 -o dataset/testing.csv
--- nlp_final/dataHandler.py
+++ nlp_final/dataHandler.py
@@ -10,7 +10,13 @@ output = namedtuple(typename="Output", field_names=["test", "train"])
 
 
 def loadData(path: Path) -> DataFrame:
-    df: DataFrame = pandas.read_csv(filepath_or_buffer=path.absolute())
+    df: DataFrame = pandas.read_csv(
+        filepath_or_buffer=path.absolute(),
+        dtype={"text": str, "author": int},
+        engine="c",
+        encoding="utf-8",
+        encoding_errors="replace",
+    )
     return df
 
 
@@ -19,3 +25,15 @@ def splitData(df: DataFrame) -> Type[tuple]:
         df, test_size=0.15, train_size=0.85, random_state=42, shuffle=True
     )
     return output(test=data[1], train=data[0])
+
+
+def reduce(df: DataFrame) -> None:
+    """Reduces to HuggingFace's dataset AutoTrain size constraints"""
+    print(df)
+
+
+dataset: Path = Path(
+    "../dataset/Gungor_2018_VictorianAuthorAttribution_data-train.csv"
+).resolve()
+df: DataFrame = loadData(path=dataset)
+reduce(df=df)
